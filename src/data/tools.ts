2dbfa0724708cb036b0346e810e0167a119837a9
imperiumdigital1999-max <imperium.digital1999@gmail.com>
2025-09-10 14:42:06 -0300

    Updated tools.ts

--- src/data/tools.ts
+++ src/data/tools.ts
@@ -1982,7 +1982,7 @@ applications: [
     },
     {
       id: '2',
-      title: 'COMO REMOVER FUNDOS DE IMAGENS EM SEGUNDOS',
+      title: 'COMO REMOVER O FUNDO DE IMAGEM | 4 Sites Gratuitos',
       thumbnail: 'https://img.youtube.com/vi/732f16D9zxY/hqdefault.jpg',
       url: 'https://www.youtube.com/watch?v=732f16D9zxY'
     }
